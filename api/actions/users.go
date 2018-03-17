8fefc52f8b93a8a49c78882f78e9f013a89cf8fe
Boris Le Meec <boris.le-meec@epitech.eu>
2018-03-17 23:40:38 +0100

    ok

---
+++ api/actions/users.go
@@ -0,0 +1,46 @@
+package actions
+
+import (
+	"github.com/BorisLeMeec/BlinkPay/api/models"
+	"github.com/satori/go.uuid"
+	"github.com/gobuffalo/buffalo"
+)
+
+var db = make(map[uuid.UUID]models.User)
+
+type UserResource struct{}
+
+func (ur UserResource) List(c buffalo.Context) error {
+	return c.Render(200, r.JSON(db))
+}
+
+func (ur UserResource) Create(c buffalo.Context) error {
+	// new User
+	user := &models.User{
+		// on génère un nouvel id
+		ID: uuid.NewV4(),
+	}
+	// add in database
+	db[user.ID] = *user
+
+	return c.Render(201, r.JSON(user))
+}
+
+func (ur UserResource) Show(c buffalo.Context) error {
+	// get id and format to uuid
+	id, err := uuid.FromString(c.Param("id"))
+	if err != nil {
+		// if id isnt uuid
+		return c.Render(500, r.String("id is not uuid v4"))
+	}
+
+	// get user in database
+	user, ok := db[id]
+	if ok {
+		// if exist return user
+		return c.Render(200, r.JSON(user))
+	}
+
+	// if not exist return not found
+	return c.Render(404, r.String("user not found"))
+}
