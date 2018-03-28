b29c143f4cff1326e03a62ccf7db1bccff8e71dd
Boris Le Meec <boris.le-meec@epitech.eu>
2018-03-29 01:59:53 +0200

    add payment functionnality

---
+++ api/actions/payment.go
@@ -0,0 +1,63 @@
+package actions
+
+import (
+	"github.com/gobuffalo/buffalo"
+	"net/http"
+	"net/url"
+	"encoding/json"
+	"io/ioutil"
+)
+
+func Pay(c buffalo.Context) error {
+	// new User
+	pictureEncoded := c.Request().FormValue("base64")
+	resp, err := http.PostForm("192.168.0.13:8080/facebox/check",
+		url.Values{"base64": {pictureEncoded}})
+	if err != nil {
+		c.Render(500, r.String("internal error"))
+	}
+	res, err := ioutil.ReadAll(resp.Body)
+	if err != nil{
+		c.Render(500, r.String("Internal Error"))
+	}
+
+	var rCheck ResponseCheck
+
+	json.Unmarshal(res, &rCheck)
+	if rCheck.Success == false || rCheck.FacesCount < 1 {
+		c.Render(401, r.JSON(ResultPayment{false, "no one recognized on the picture",
+				1, nil, nil}))
+	}
+	return c.Render(200, r.JSON(ResultPayment{true, nil, nil, 0,
+	rCheck.Faces[0].ID}))
+}
+
+type ResponseCheck struct {
+	Success bool `json:"success"`
+	FacesCount int `json:"facesCount"`
+	Faces []Face `json:"faces"`
+}
+
+type Face struct {
+	Rect Rectangle `json:"rect"`
+	ID_pic string `json:"id"`
+	ID string `json:"name"`
+	Matched bool `json:"matched"`
+	Confidence float64 `json:"confidence"`
+}
+
+type Rectangle struct {
+	Top int `json:"top"`
+	Left int `json:"left"`
+	Width int `json:"width"`
+	Height int `json:"height"`
+}
+
+type ResultPayment struct {
+	Success bool `json:"success"`
+	Error string `json:"error"`
+	ErrorID int `json:"errorId"`
+	TransactionID int `json:"transactionId"`
+	Name string `json:"name"`
+
+}
